befffc6c7139d99250d9f78021da734464f4e68d
mshahpazova <maria.shahpazova@xoomworks.com>
2016-08-11 11:08:08 +0300

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,37 +9,41 @@ var sourcemaps = require('gulp-sourcemaps');
 var server = require('gulp-server-livereload');
 var connect = require('gulp-connect');
 
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
     return gulp.src('app/**/*.ts')
-    .pipe(
-        ts({
-            "compilerOptions": {
-                 "target": "es5",
-                 "module": "commonjs",
-                 "sourceMap": true
-             }
-        })
-    )
-    .pipe(gulp.dest(function(file) {
-        return file.base;
-    }));
+        .pipe(
+            ts({
+                // "compilerOptions": {
+                "target": "es5",
+                "module": "commonjs",
+                "sourceMap": true
+                    // }
+            })
+        )
+        .pipe(gulp.dest(function(file) {
+            return file.base;
+        }));
 });
 
 gulp.task('watch', function() {
-  // Watch .ts files
-  gulp.watch('app/**/*.ts', ['scripts']);
+    // Watch .ts files
+    gulp.watch('app/**/*.ts', ['scripts']);
 });
 
-gulp.task('watch', function() {
-  // Watch .ts files
-  gulp.watch('app/app.ts', ['scripts']);
-});
+// gulp.task('watch', function() {
+//     // Watch .ts files
+//     gulp.watch('app/app.ts', ['scripts']);
+// });
 
 gulp.task('webserver', function() {
-  connect.server({
-    livereload: true,
-    port: 8447
-  });
+    connect.server({
+        livereload: true,
+        port: 8447
+    });
 });
 
-gulp.task('default', ['scripts', 'watch', 'webserver']);
+gulp.task('hello', function() {
+    console.log("Hello, Maria :)")
+})
+
+gulp.task('default', ['hello', 'scripts', 'watch', 'webserver']);
